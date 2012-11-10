ae995e11d3f443f02abc9d6fc723da1ccf8397f4
Cooper Riley <rileycr@muohio.edu>
2012-11-10 00:39:33 -0500

    Now displays points and implimented getNearest()

--- src/HW04_Phase3App.cpp
+++ src/HW04_Phase3App.cpp
@@ -23,16 +23,19 @@ public:
 private:
 	rileycrStarbucks* dataTree_;
 	Entry* dataFile_;
+	Entry* nearest_;
 	uint8_t* dataArray_;
 	Color8u locationColor;
 	int windowWidth;
 	int windowHeight;
+	gl::Texture usMap_;
 };
 
 void HW04_Phase3App::setup(){
-
+	usMap_ = gl::Texture(loadImage( "usMap.png"));
 	dataFile_ = new Entry[8000];
 	ifstream in("Starbucks_2006.csv");
+	nearest_ = new Entry();
 
 	for(int i = 0; i < 7655; i++){
 		getline(in, dataFile_[i].identifier, ',');
@@ -50,7 +53,23 @@ void HW04_Phase3App::setup(){
 }
 
 void HW04_Phase3App::mouseDown( MouseEvent event ){
+	Vec2i mouse_location = event.getPos();
+	double scaledXValue = mouse_location.x/(getWindowWidth()*1.065);
+	double scaledYValue = mouse_location.y/(getWindowHeight()*.96);
 
+	//Entry temp = dataFile_[0];
+	//for(int i=0; i < 7655; i++){
+	//	if(sqrt((dataFile_[i].x-scaledXValue)*(dataFile_[i].x-scaledXValue)+
+	//		(dataFile_[i].y-scaledYValue)*(dataFile_[i].y-scaledYValue))
+	//		< sqrt((temp.x-scaledXValue)*(temp.x-scaledXValue)+
+	//		(temp.y-scaledYValue)*(temp.y-scaledYValue))){
+	//			temp = dataFile_[i];
+	//	}
+	//}
+	//*nearest_ = temp;
+
+	nearest_ = dataTree_ -> getNearest(scaledXValue, scaledYValue);
+	console() << nearest_ ->x << endl;
 }
 
 void HW04_Phase3App::update(){
@@ -58,10 +77,18 @@ void HW04_Phase3App::update(){
 }
 
 void HW04_Phase3App::draw(){
-	gl::clear( Color(255,255,255) );
+	gl::color( Color(255,255,255));
+	gl::draw( usMap_, getWindowBounds() );
+
+	//gl::color(Color8u(255,255,255));
+
+	if(nearest_ -> x >= 0){
+		gl::drawSolidCircle(Vec2f(nearest_ -> x*getWindowWidth()*1.065, nearest_ -> y*getWindowHeight()*.96), 3);
+	}
 	gl::color(Color8u(0,0,0));
 	for(int i = 0; i < 7655; i++){
-		gl::drawSolidCircle(Vec2f(dataFile_[i].x*windowWidth, windowHeight - dataFile_[i].y*windowHeight), 4);
+		gl::drawSolidCircle(Vec2f(dataFile_[i].x*(getWindowWidth()*1.065),
+			windowHeight - dataFile_[i].y*(getWindowHeight()*.96)), 1);
 	}
 }
 
@@ -70,7 +97,7 @@ void HW04_Phase3App::prepareSettings(Settings *settings){
 	windowHeight = 750;
 	settings -> setWindowPos(7,29);
 	settings -> setWindowSize(windowWidth, windowHeight);
-	
+
 }
 
 CINDER_APP_BASIC( HW04_Phase3App, RendererGl )
